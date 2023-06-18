1bb5af986affb86cea0f740ce7c5e8ce2d0a72ef
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-06-18 15:36:58 -0700

    initial functions and event listner added

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("done")
+//Elements
+const cakesSelect = document.querySelector("#cakes")
+
+//Functions
+getCakes()
+
+//Event listeners
+
+cakesSelect.addEventListener("change", selectaCake)
+
+function getCakes() {
+  fetch("http://localhost:3000/cakes")
+    .then((res) => res.json())
+    .then((cakes) => renderCakeOptions(cakes))
+    .catch((error) => alert(error))
+}
+
+function renderCakeOptions(cakes) {
+  cakes.forEach((cake) => {
+    const option = document.createElement("option")
+    option.value = cake.name
+    option.textContent = cake.name
+    cakesSelect.append(option)
+  })
+}
